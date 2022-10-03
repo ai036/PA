5b6c1a09f5e7ca8917a896e4c34b361c06c06deb
tracer-ics2022 <tracer@njuics.org>
2022-10-03 23:49:17 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:49:17 up  1:30,  1 user,  load average: 0.50, 0.52, 0.55

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -94,7 +94,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
   bool a;
   expr(args,&a);
-  printf("%d\n",eval(0,re_nr_token()-1));
+  printf("  %d\n",eval(0,re_nr_token()-1));
   //return eval(0,re_nr_token()-1);
   return 0;
 }
