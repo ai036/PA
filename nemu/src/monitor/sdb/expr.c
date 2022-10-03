06306af1148b12c3731e3f924b75c5174e1a8a6a
tracer-ics2022 <tracer@njuics.org>
2022-10-03 14:20:48 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:20:48 up 35 min,  1 user,  load average: 0.50, 0.38, 0.31

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -184,7 +184,7 @@ bool check_parentheses(int p, int q)
 int eval(int p, int q) {
   if (p > q) {
     /* Bad expression */
-    printf("Wrong expression.");
+    printf("Wrong expression.\n");
     return -1;
   }
   else if (p == q) {
