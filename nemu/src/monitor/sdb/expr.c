9b274e311673f0f9243e0418354b2a71400cf65b
tracer-ics2022 <tracer@njuics.org>
2022-10-06 16:07:37 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:07:37 up 1 min,  1 user,  load average: 1.70, 0.89, 0.34

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -122,18 +122,19 @@ static bool make_token(char *e) {
           case TK_EQ: tokens[nr_token].type=TK_EQ;break;
           case TK_NEQ: tokens[nr_token].type=TK_NEQ;break;
           case TK_HEX: tokens[nr_token].type=TK_HEX;
-            printf("%s",substr_start);
-          break;
+            for(int k=0;k<substr_len;k++)
+              tokens[nr_token].str[k]=substr_start[k];
+            break;
           case TK_NUM: tokens[nr_token].type=TK_NUM;
             for(int k=0;k<substr_len;k++)
               tokens[nr_token].str[k]=substr_start[k];
             break;
-          
           case TK_LEFT: tokens[nr_token].type=TK_LEFT;break;
           case TK_RIGHT: tokens[nr_token].type=TK_RIGHT;break;
           case TK_NOTYPE: break;
           case TK_AND: tokens[nr_token].type=TK_AND;break;
           case TK_REG: tokens[nr_token].type=TK_REG;
+            
           break;
 
           default: TODO();
