b6b5ec9fe6395b37698ec2a80100aca5b290f9e8
tracer-ics2022 <tracer@njuics.org>
2022-10-07 19:35:35 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:35:35 up 33 min,  1 user,  load average: 0.14, 0.37, 0.42

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -61,6 +61,7 @@ int new_wp(char *str)
       { printf("head::%s\n",head->expr);
         tail=tail->next;
       }tail->next=p;                  //从链表尾插入
+      printf("head::%s\n",head->expr);
     }
  // num++;
   p->expr=str;
