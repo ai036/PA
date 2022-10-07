4731a9536300c9fc1af87154ecda319bb08429b0
tracer-ics2022 <tracer@njuics.org>
2022-10-07 19:29:31 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-48-generic #54-Ubuntu SMP Fri Aug 26 13:26:29 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:29:31 up 27 min,  1 user,  load average: 0.69, 0.67, 0.51

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -64,7 +64,7 @@ int new_wp(char *str)
  // num++;
   p->expr=str;
   p->value=expr(str,&a);
-  printf("Hardware watchpoint %d: %s\n",p->NO,head->expr);
+  printf("Hardware watchpoint!!! %d: %s\n",p->NO,head->expr);
   p->next=NULL;
   return 0;
     
