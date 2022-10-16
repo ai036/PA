0d61f049771f15461cc2ae968f1f19ac0139effc
tracer-ics2022 <tracer@njuics.org>
2022-10-16 16:07:53 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:07:53 up  3:05,  1 user,  load average: 0.47, 0.40, 0.37

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -27,9 +27,9 @@ void isa_reg_display() {
   printf("The states of all General Purpose Registers:\n");
   for(int i = 0; i <32; i++)
     {if(i==26||i==27)
-       printf("%s  %p   %d     ",regs[i],&cpu.gpr[i],cpu.gpr[i]);
+       printf("%s  %p   %u     ",regs[i],&cpu.gpr[i],cpu.gpr[i]);
      else
-       printf("%s   %p   %d     ",regs[i],&cpu.gpr[i],cpu.gpr[i]);
+       printf("%s   %p   %u     ",regs[i],&cpu.gpr[i],cpu.gpr[i]);
      if((i+1)%2==0)
         printf("\n");
     }
