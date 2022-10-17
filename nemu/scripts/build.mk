e793088a958ec5e0ce285001ee1387a50a2af6ce
tracer-ics2022 <tracer@njuics.org>
2022-10-17 13:02:21 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-50-generic #56-Ubuntu SMP Tue Sep 20 13:23:26 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:02:21 up 5 min,  1 user,  load average: 0.46, 0.64, 0.33

--- nemu/scripts/build.mk
+++ nemu/scripts/build.mk
@@ -33,9 +33,6 @@ $(OBJ_DIR)/%.o: %.c
 	@echo + CC $<
 	@mkdir -p $(dir $@)
 	@$(CC) $(CFLAGS) -c -o $@ $<
-	@$(CC) $(CFLAGS) $(SO_CFLAGS) -E -MF /dev/null $< | \
-		grep -ve '^#' | \
-		clang-format - > $(basename $@).i
 	$(call call_fixdep, $(@:.o=.d), $@)
 
 $(OBJ_DIR)/%.o: %.cc
