b237092139cf45e4306c499bfec80f2835d524f3
tracer-ics2022 <tracer@njuics.org>
2022-10-25 15:10:34 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:10:34 up  1:07,  1 user,  load average: 0.85, 0.72, 0.60

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -34,8 +34,8 @@ NEMU_EXEC := $(BINARY) $(ARGS) $(IMG)
 run-env: $(BINARY) $(DIFF_REF_SO)
 
 run: run-env
-	$(call git_commit, "run NEMU")
 
+	$(NEMU_EXEC)
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
