6369d45cbfa3e1f5c808b5d5bf3180ee0aa2f4e0
tracer-ics2022 <tracer@njuics.org>
2022-10-25 08:58:01 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:58:01 up 26 min,  1 user,  load average: 0.98, 0.76, 0.62

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -82,9 +82,10 @@ static void exec_once(Decode *s, vaddr_t pc) {
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);  
   
-  printf("%s hhhhhhh  ",q);
   iringbuf.instruction[iringbuf.no]=q;
   iringbuf.no=(iringbuf.no+1)%20;
+  for(int i=0; i<iringbuf.no;i++)
+    printf("the ith instruction %s\n",iringbuf.instruction[i]);
 
 #endif
 }
