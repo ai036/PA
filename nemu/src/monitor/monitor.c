ac98e34c81b185e2985e1e5c785a5bf994eb1517
tracer-ics2022 <tracer@njuics.org>
2022-10-26 13:20:29 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:20:29 up 46 min,  1 user,  load average: 0.66, 0.71, 0.46

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -143,12 +143,7 @@ void init_monitor(int argc, char *argv[]) {
 	}
 
 	// 设置fp偏移量 offset，e_shoff含义
-	a = fseek(elf, elf_head.e_shoff, SEEK_SET); //fseek调整指针的位置，采用参考位置+偏移量
-	if (0 != a)
-	{
-		printf("\nfaile to fseek\n");
-		exit(0);
-	}
+
 
 	// 读取section 到 shdr, 大小为shdr * 数量
 	a = fread(shdr, sizeof(Elf64_Shdr) * elf_head.e_shnum, 1, elf);
@@ -180,7 +175,7 @@ void init_monitor(int argc, char *argv[]) {
 	{
 		temp = shstrtab;
 		temp = temp + shdr[i].sh_name;
-        if (strcmp(temp, ".text") != 0) continue;//该section名称
+        if (strcmp(temp, ".strtab") != 0) continue;//该section名称
 		printf("节的名称: %s\n", temp);
 		printf("节首的偏移: %lx\n", shdr[i].sh_offset);
 		printf("节的大小: %lx\n", shdr[i].sh_size);
