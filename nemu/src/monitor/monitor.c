e240ffe3fd7a1945d721672b58b7377de2670a5b
tracer-ics2022 <tracer@njuics.org>
2022-10-26 12:50:08 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:50:08 up 15 min,  1 user,  load average: 0.18, 0.31, 0.26

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -100,82 +100,11 @@ static int parse_args(int argc, char *argv[]) {
   return 0;
 }
 
-
-
-static char *strtab = NULL;
-static Elf32_Sym *symtab = NULL;
-static int nr_symtab_entry;
-
-void load_elf_tables(int argc, char *argv[]) {
-	int ret;
-
-	//exec_file = argv[1];
-
-	FILE *fp = fopen("hello-world", "rb");
-
-
-	uint8_t buf[sizeof(Elf32_Ehdr)];
-	ret = fread(buf, sizeof(Elf32_Ehdr), 1, fp);
-	assert(ret == 1);
-
-	/* The first several bytes contain the ELF header. */
-	Elf32_Ehdr *elf = (void *)buf;
-
-
-
-	/* Load symbol table and string table for future use */
-
-	/* Load section header table */
-	uint32_t sh_size = elf->e_shentsize * elf->e_shnum;
-	Elf32_Shdr *sh = malloc(sh_size);
-	fseek(fp, elf->e_shoff, SEEK_SET);
-	ret = fread(sh, sh_size, 1, fp);
-	assert(ret == 1);
-
-	/* Load section header string table */
-	char *shstrtab = malloc(sh[elf->e_shstrndx].sh_size);
-	fseek(fp, sh[elf->e_shstrndx].sh_offset, SEEK_SET);
-	ret = fread(shstrtab, sh[elf->e_shstrndx].sh_size, 1, fp);
-	assert(ret == 1);
-
-	int i;
-	for(i = 0; i < elf->e_shnum; i ++) {
-		if(sh[i].sh_type == SHT_SYMTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0) {
-			/* Load symbol table from exec_file */
-			symtab = malloc(sh[i].sh_size);
-			fseek(fp, sh[i].sh_offset, SEEK_SET);
-			ret = fread(symtab, sh[i].sh_size, 1, fp);
-			assert(ret == 1);
-			nr_symtab_entry = sh[i].sh_size / sizeof(symtab[0]);
-		}
-		else if(sh[i].sh_type == SHT_STRTAB && 
-				strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
-			/* Load string table from exec_file */
-			strtab = malloc(sh[i].sh_size);
-			fseek(fp, sh[i].sh_offset, SEEK_SET);
-			ret = fread(strtab, sh[i].sh_size, 1, fp);
-			assert(ret == 1);
-		}
-	}
-
-	free(sh);
-	free(shstrtab);
-
-	assert(strtab != NULL && symtab != NULL);
-
-	fclose(fp);
-}
-
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
 
   /* Parse arguments. */
   parse_args(argc, argv);
-  
-  load_elf_tables(argc, argv);
-  printf("%s",strtab);
-  /*
   elf = fopen("hello-world","r");
   if (NULL == elf)
 	{
@@ -267,7 +196,7 @@ void init_monitor(int argc, char *argv[]) {
 		    printf("%x", *p);
             p++;
 		}
-	 }*/
+	 }
 
   /* Set random seed. */
   init_rand();
@@ -321,5 +250,3 @@ void am_init_monitor() {
   welcome();
 }
 #endif
-
-
