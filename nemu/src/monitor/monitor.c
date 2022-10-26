5d33d13b6a10d8b41db219807e48c079b46e2d86
tracer-ics2022 <tracer@njuics.org>
2022-10-26 09:29:42 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:29:42 up 21 min,  1 user,  load average: 0.11, 0.41, 0.43

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -15,6 +15,7 @@
 
 #include <isa.h>
 #include <memory/paddr.h>
+#include <elf.h>
 
 void init_rand();
 void init_log(const char *log_file);
@@ -44,6 +45,7 @@ static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
+FILE* elf = NULL;
 
 static long load_img() {
   if (img_file == NULL) {
@@ -84,6 +86,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1: img_file = optarg; return 0;
+      case 2: elf = fopen(optarg, "r");break;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
@@ -102,6 +105,98 @@ void init_monitor(int argc, char *argv[]) {
 
   /* Parse arguments. */
   parse_args(argc, argv);
+
+  if (NULL == elf)
+	{
+		printf("fail to open the file");
+		exit(0);
+	}
+
+	// 解析head
+	Elf64_Ehdr elf_head;
+	int  a;
+
+	// 读取 head 到elf_head
+	a = fread(&elf_head, sizeof(Elf64_Ehdr), 1, elf);   //fread参数1：读取内容存储地址，参数2：读取内容大小，参数3：读取次数，参数4：文件读取引擎
+	if (0 == a)
+	{
+		printf("fail to read head\n");
+		exit(0);
+	}
+
+	// 判断elf文件类型
+	if (elf_head.e_ident[0] != 0x7F ||
+		elf_head.e_ident[1] != 'E' ||
+		elf_head.e_ident[2] != 'L' ||
+		elf_head.e_ident[3] != 'F')
+	{
+		printf("Not a ELF file\n");
+		exit(0);
+	}
+
+	// 解析section 分配内存 section * 数量
+	Elf64_Shdr *shdr = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr) * elf_head.e_shnum);
+	if (NULL == shdr)
+	{
+		printf("shdr malloc failed\n");
+		exit(0);
+	}
+
+	// 设置fp偏移量 offset，e_shoff含义
+	a = fseek(elf, elf_head.e_shoff, SEEK_SET); //fseek调整指针的位置，采用参考位置+偏移量
+	if (0 != a)
+	{
+		printf("\nfaile to fseek\n");
+		exit(0);
+	}
+
+	// 读取section 到 shdr, 大小为shdr * 数量
+	a = fread(shdr, sizeof(Elf64_Shdr) * elf_head.e_shnum, 1, elf);
+	if (0 == a)
+	{
+		printf("\nfail to read section\n");
+		exit(0);
+	}
+
+	// 重置指针位置到文件流开头
+	rewind(elf);
+
+	// 将fp指针移到 字符串表偏移位置处
+	fseek(elf, shdr[elf_head.e_shstrndx].sh_offset, SEEK_SET);
+
+	// 第e_shstrndx项是字符串表 定义 字节 长度 char类型 数组
+	char shstrtab[shdr[elf_head.e_shstrndx].sh_size];
+	char *temp = shstrtab;
+
+	// 读取内容
+	a = fread(shstrtab, shdr[elf_head.e_shstrndx].sh_size, 1, elf);
+	if (0 == a)
+	{
+		printf("\nfaile to read\n");
+	}
+    
+	// 遍历
+	for (int i = 0; i < elf_head.e_shnum; i++)
+	{
+		temp = shstrtab;
+		temp = temp + shdr[i].sh_name;
+        if (strcmp(temp, ".dynsym") != 0) continue;//该section名称
+		printf("节的名称: %s\n", temp);
+		printf("节首的偏移: %lx\n", shdr[i].sh_offset);
+		printf("节的大小: %lx\n", shdr[i].sh_size);
+        uint8_t *sign_data=(uint8_t*)malloc(sizeof(uint8_t)*shdr[i].sh_size);
+		// 依据此段在文件中的偏移读取出
+		fseek(elf, shdr[i].sh_offset, SEEK_SET);
+		a=fread(sign_data, sizeof(uint8_t)*shdr[i].sh_size, 1, elf);
+		// 显示读取的内容
+		uint8_t *p = sign_data;
+		int j = 0;
+		for (j=0; j<shdr[i].sh_size; j++)
+		{
+		    printf("%x", *p);
+            p++;
+		}
+	 }
 
   /* Set random seed. */
   init_rand();
