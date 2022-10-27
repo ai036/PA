dd215ab565f6ecb8619a4b551d7173cbf75db75e
tracer-ics2022 <tracer@njuics.org>
2022-10-27 09:56:29 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:56:29 up 14 min,  1 user,  load average: 0.28, 0.37, 0.34

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -89,7 +89,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1: img_file = optarg; return 0;
-      case 2: elfp = fopen(optarg, "r");break;
+      case 'f': elfp = fopen(optarg, "r");break;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
