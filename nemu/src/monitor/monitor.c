54f45a767129a19912b897ebf1fa485b452573a8
tracer-ics2022 <tracer@njuics.org>
2022-10-27 10:02:16 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:02:16 up 20 min,  1 user,  load average: 0.17, 0.28, 0.32

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -110,7 +110,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Parse arguments. */
   parse_args(argc, argv);
   
-  printf("%s\n",argv[4]);
+  printf("%s\n",argv[3]);
 
   /* Set random seed. */
   init_rand();
