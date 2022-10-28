d6774f06fa1876b8481ffb19d1a1aa0737652422
tracer-ics2022 <tracer@njuics.org>
2022-10-28 13:12:58 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:12:58 up 24 min,  1 user,  load average: 0.65, 0.65, 0.54

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -16,10 +16,15 @@
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
+extern CPU_state cpu;
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  
-  return false;
+  for(int i=0;i<32;i++)
+    if(ref_r->gpr[i]!=cpu.gpr[i])
+      return false;
+  if(pc!=cpu.pc)
+    return false;
+  return true;
 }
 
 void isa_difftest_attach() {
