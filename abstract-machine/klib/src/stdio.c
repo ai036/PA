ca95524f0c45a67273d65f2be31ae626311818c2
tracer-ics2022 <tracer@njuics.org>
2022-10-30 14:50:44 +0800

    >  compile NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:50:44 up 35 min,  1 user,  load average: 0.86, 0.76, 0.62

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,14 +5,6 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
-}
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
-}
-
 //将十进制数num转换为base进制的字符串
 int convert(char *out,int end,int num,int base)
 { 
@@ -50,6 +42,52 @@ int cat(char *out, char *src,int end)
 {
   while(*src!='\0')
     out[end++]=*src++;
+  out[end]='\0';
+  return end;
+}
+
+int printf(const char *fmt, ...) {
+//  panic("Not implemented");
+  char buf[1024];
+  va_list args;
+  va_start(args, fmt);
+  vsprintf(buf,fmt, args);
+  int i=0;
+  while(buf[i]!='\0') 
+    putch(buf[i++]);
+  return i;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+//  panic("Not implemented");
+  size_t len=strlen(fmt);
+  int end=0;
+  int arg_int=0;
+  char *arg_str=NULL;
+  for(int i=0;i<len;i++)
+    {
+      if(fmt[i]!='%')
+        out[end++]=fmt[i];
+      else if(i+1<len)//后面是需要格式化输出的内容
+      {
+        switch(fmt[i+1])
+        { 
+          case 's':
+          arg_str=va_arg(ap,char*);
+          end=cat(out,arg_str,end);
+          i++;
+          break;
+          
+          case 'd':
+          arg_int=va_arg(ap,int);
+          end=convert(out,end,arg_int,10);
+          i++;
+          break;    
+        }
+      }
+      else
+        out[end++]=fmt[i];
+    }
   out[end]='\0';
   return end;
 }
