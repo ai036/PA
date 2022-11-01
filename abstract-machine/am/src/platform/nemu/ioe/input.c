367c3f445e1e3f8ed0b8eb571f9fcfb5a82917d4
tracer-ics2022 <tracer@njuics.org>
2022-11-01 21:00:21 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:00:21 up 41 min,  1 user,  load average: 0.36, 0.31, 0.29

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,7 +4,12 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
+  if(inl(KBD_ADDR)!=AM_KEY_NONE)
+    {kbd->keydown = 1;
+     kbd->keycode = inl(KBD_ADDR);
+     }
+  else{
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
-  
+  }
 }
