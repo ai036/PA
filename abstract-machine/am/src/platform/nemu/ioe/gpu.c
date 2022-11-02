803a2b6c93d5602abf1efa7f2fc9c830c2387732
tracer-ics2022 <tracer@njuics.org>
2022-11-02 12:17:24 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:17:24 up  2:02,  1 user,  load average: 1.18, 1.04, 0.79

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,7 +23,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;//(x,y)像素坐标，(w,h)像素宽高
   uint32_t *pixel= (uint32_t *)ctl->pixels;
   fb=fb+y*W+x;
-  for (int i = 0; i < h; i ++)
+  for (int i = 0; i < h; i++)
     for(int j=0;j<w;j++)
       fb[i*W+j]=pixel[i*w+j];
   if (ctl->sync) {
