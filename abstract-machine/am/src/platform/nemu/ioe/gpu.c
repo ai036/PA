8c375ae609896edbfa401d4fa59612e5f4c6b798
tracer-ics2022 <tracer@njuics.org>
2022-11-02 11:56:42 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-52-generic #58-Ubuntu SMP Thu Oct 13 08:03:55 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:56:42 up  1:41,  1 user,  load average: 0.42, 0.60, 0.55

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,7 +6,12 @@
 #define WIDTH_ADDR (VGACTL_ADDR)
 
 void __am_gpu_init() {
-
+  int i;
+  int w = inw(WIDTH_ADDR);  // TODO: get the correct width
+  int h = inw(HEIGHT_ADDR);  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -24,10 +29,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *pixel= (uint32_t *)ctl->pixels;
   if(h==0||w==0)
     return;
-  fb=fb+y*w+x;
+  fb=fb+y*W+x;
   for (int i = 0; i < h; i ++)
     for(int j=0;j<w;j++)
-      outl(FB_ADDR+(y*W+x+i*w+j)*4,pixel[0]);
+//      outl(FB_ADDR+(y*W+x+i*w+j)*4,pixel[0]);
+        fb[i*w+j]=pixel[i*w+j];
 
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
