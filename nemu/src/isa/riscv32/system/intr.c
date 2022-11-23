02334ae81314a5a909c1922efdf2e637dc76ffc5
tracer-ics2022 <tracer@njuics.org>
2022-11-23 16:59:08 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:59:08 up 59 min,  1 user,  load average: 1.31, 0.92, 0.66

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -20,9 +20,9 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
-  set_nemu_state(NEMU_STOP,epc,NO);
 
-  return epc;
+
+  return 0;
 }
 
 word_t isa_query_intr() {
