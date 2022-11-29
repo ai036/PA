d4156e7ff4df8e0c5ef439a1df3fef40bf245521
tracer-ics2022 <tracer@njuics.org>
2022-11-29 23:10:31 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:10:31 up 24 min,  1 user,  load average: 0.65, 0.43, 0.35

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,7 +13,7 @@ size_t ramdisk_read(void *buf, size_t offset, size_t len);
 Elf_Ehdr elf;
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();             //把用户程序加载到正确的内存位置
+          //把用户程序加载到正确的内存位置
 
   Elf32_Ehdr elf_head;
   ramdisk_read(&elf_head,0, sizeof(Elf32_Ehdr));
