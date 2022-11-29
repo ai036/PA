e6ee9997356f1361eb773d77dc89faf26dc6744d
tracer-ics2022 <tracer@njuics.org>
2022-11-29 23:09:58 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:09:58 up 24 min,  1 user,  load average: 0.51, 0.39, 0.33

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -6,6 +6,8 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used)) = {};
 static PCB pcb_boot = {};
 PCB *current = NULL;
 
+void naive_uload(PCB *pcb, const char *filename);
+
 void switch_boot_pcb() {
   current = &pcb_boot;
 }
@@ -20,7 +22,7 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-
+  naive_uload(NULL,NULL);
   switch_boot_pcb();
 
   Log("Initializing processes...");
