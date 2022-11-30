a2893c1fb79755c646aa8ccd36d41f7abdbd753b
tracer-ics2022 <tracer@njuics.org>
2022-11-30 10:43:49 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:43:49 up 39 min,  1 user,  load average: 0.73, 0.46, 0.39

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,13 +9,9 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0: ev.event = EVENT_NULL; break;
-      case 1: ev.event = EVENT_YIELD; break;
-      case 2: ev.event = EVENT_SYSCALL; break;
-      case 3: ev.event = EVENT_PAGEFAULT; break;
+
       case 11: ev.event = EVENT_YIELD; break;
-      case 5: ev.event = EVENT_IRQ_TIMER; break;
-      case 6: ev.event = EVENT_IRQ_IODEV; break;
+
       default: ev.event = EVENT_ERROR; break;
     }
   printf("cause: %d\n",c->mcause);
