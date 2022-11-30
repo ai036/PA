3237173f8be13652913021c2726b2271b79cb46c
tracer-ics2022 <tracer@njuics.org>
2022-11-30 14:31:02 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-53-generic #59-Ubuntu SMP Mon Oct 17 18:53:30 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:31:02 up  2:05,  1 user,  load average: 1.14, 0.73, 0.59

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,5 +1,16 @@
 #include <common.h>
 #include "syscall.h"
+
+struct SYS_TRACE{
+    char *name;
+    int perimeter[4];
+    int ret;
+  };
+#ifdef STRACE
+  SYS_TRACE strace[20];
+  int sys_index=0;
+#endif
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -9,4 +20,13 @@ void do_syscall(Context *c) {
     case 1: yield(); c->GPRx=0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
+  
+#ifdef STRACE
+  strace[sys_index].name="syscall";
+  strace[sys_index].perimeter[0]=c->GPR1;
+  strace[sys_index].perimeter[1]=c->GPR2;
+  strace[sys_index].perimeter[2]=c->GPR3;
+  strace[sys_index].perimeter[3]=c->GPR4;
+  strace[sys_index].ret=c->GPRx;
+#endif
 }
