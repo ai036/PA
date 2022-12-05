b1f76b4bec8dd862e78dcc1ad457e96176aecbff
tracer-ics2022 <tracer@njuics.org>
2022-12-05 21:16:57 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:16:57 up  2:04,  1 user,  load average: 0.58, 0.46, 0.49

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,9 +80,6 @@ size_t fs_write(int fd, const void *buf, size_t len)
      }
     printf("open_offset:%d\n",file_table[fd].open_offset);
   printf("size:%d\n",file_table[fd].size);
-  char f[10000];
-  ramdisk_read(f,file_table[fd].disk_offset,5000);
-  printf("%s\n",f);
   return ret;
 }
 
