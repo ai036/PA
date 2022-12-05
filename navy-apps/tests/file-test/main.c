22baacfeeeafb155b20c2119f9c0f227cd6f8d80
tracer-ics2022 <tracer@njuics.org>
2022-12-05 21:33:59 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:33:59 up  2:21,  1 user,  load average: 0.65, 0.83, 0.73

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -26,7 +26,7 @@ int main() {
     assert(n == i + 1);
   }
 
-  fseek(fp, 999, SEEK_SET);
+  fseek(fp, 1002, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
     printf("%dth ",i);
