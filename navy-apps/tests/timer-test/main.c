4625a79aefe9bc3af50ba705ab28a663954f2102
tracer-ics2022 <tracer@njuics.org>
2022-12-06 14:40:14 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:40:14 up 51 min,  1 user,  load average: 0.52, 0.52, 0.46

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -10,14 +10,12 @@ int main(){
   struct timeval tv;
   int ret=gettimeofday(&tv,NULL);
 
-  uint32_t time;
+  uint32_t time=NDL_GetTicks();
   uint32_t msec = 500;
   printf("%u\n",time);
   while (1) {
     while(time < msec) {
-  	ret=gettimeofday(&tv,NULL);
-  	time=tv.tv_sec*1000+tv.tv_usec/1000;
-
+      uint32_t time=NDL_GetTicks();
     };
     printf("time pass 0.5s\n");
 
