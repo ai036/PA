970c90e9acdceb67d3a0ffdbb0855ba3664f2987
tracer-ics2022 <tracer@njuics.org>
2022-12-07 16:19:57 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:19:57 up 22 min,  1 user,  load average: 1.34, 0.95, 0.67

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -87,7 +87,7 @@ int NDL_Init(uint32_t flags) {
   char b[128];
   int end=0;
   for(int i=0;i<128;i++)
-    if(buf[i]!=' ')
+    if(buf[i]!=' '&&buf[i]!='\0')
       b[end++]=buf[i];
   b[end]='\0';
   printf("%s\n",b);
