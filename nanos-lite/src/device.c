4f0f5d61b4629fb9badf8650439d3c968b9b1502
tracer-ics2022 <tracer@njuics.org>
2022-12-07 11:39:43 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:39:43 up  1:37,  1 user,  load average: 0.51, 0.55, 0.48

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -44,7 +44,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   int height=io_read(AM_GPU_CONFIG).height;
   printf("(%d,%d)\n",width,height);
   int ret=sprintf(buf,"WIDTH:%d\nHEIGHT:%d",width,height);
-  assert(ret<=len);
+  if(ret)
   printf("111\n");
   return 0;
 }
