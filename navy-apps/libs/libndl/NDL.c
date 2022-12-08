37bc0fd35ad5ecf3c95788780b7eee1d7a458836
tracer-ics2022 <tracer@njuics.org>
2022-12-08 19:07:17 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:07:17 up 40 min,  1 user,  load average: 1.13, 0.65, 0.51

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -66,6 +66,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
 
   canvas_x=(screen_w-canvas_w)/2;
   canvas_y=(screen_h-canvas_h)/2;
+  printf("width:%d height:%d\n",canvas_w,canvas_h);
   offset = canvas_y*screen_w+canvas_x+ y*screen_w+x;
   for(int i=0;i<h;i++)
   {
