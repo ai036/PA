46cb7d2d666c315333efbbe060343fd7b6db483e
tracer-ics2022 <tracer@njuics.org>
2022-12-10 00:17:58 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:17:58 up 7 min,  1 user,  load average: 0.69, 0.62, 0.34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,7 +42,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, "/bin/slider");
+  uintptr_t entry = loader(pcb, "/bin/nslider");
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
