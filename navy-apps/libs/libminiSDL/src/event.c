69af7e530910143ffc525c462c60e9e421f0e1bb
tracer-ics2022 <tracer@njuics.org>
2022-12-10 00:35:51 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:35:51 up 25 min,  1 user,  load average: 0.26, 0.36, 0.35

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -17,6 +17,12 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  char buf[100];
+  NDL_PoolEvent(buf,20);
+  char type[10];
+  char key[10];
+  sscanf(buf,"%s %s",type,key);
+  printf("%s %s\n",type,key);
   return 1;
 }
 
