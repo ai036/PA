060ed294f4a104b0c15e329f900c406885d7ece5
tracer-ics2022 <tracer@njuics.org>
2022-12-12 00:12:13 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:12:13 up 24 min,  1 user,  load average: 0.92, 0.60, 0.37

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -28,7 +28,30 @@ int SDL_WaitEvent(SDL_Event *event) {
   else if(strcmp(type,"ku")==0)
     event->type = SDL_KEYUP;
 
-
+  switch(strlen(key))
+  {
+    case 1:
+      char c=key[0];
+      if(c>='1'&&c<='9')
+        event-> key.keysym.sym= 15+c-'1';
+      else if(c=='0')
+        event->key.keysym.sym= SDLK_0;
+      else if(c=='J')
+        event->key.keysym.sym=SDLK_J;
+      else if(c=='K')
+        event->key.keysym.sym=SDLK_K;
+      else if(c=='G')
+        event->key.keysym.sym=SDLK_G;
+      break;
+    case 2:
+      event->key.keysym.sym=SDLK_UP;
+      break;
+    case 4:
+      event->key.keysym.sym=SDLK_DOWN;
+      break;
+    default:
+      event->key.keysym.sym=SDLK_NONE;
+  }
 
   printf("type:%s key:%s\n",type,key);
   return 1;
