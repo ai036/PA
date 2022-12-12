1a21fb854cfe9269a17038e7a2ad555d4342f4b8
tracer-ics2022 <tracer@njuics.org>
2022-12-12 09:51:51 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:51:51 up 28 min,  1 user,  load average: 0.47, 0.63, 0.60

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -14,7 +14,53 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  ev->type=SDL_KEYUP;
+  char buf[100];
+  char type[10];
+  char key[10];
+
+  NDL_PollEvent(buf,20);
+  sscanf(buf,"%s %s",type,key);
+  if(type[1]=='u')
+    return 0;
+  else if(type[1]=='d')
+    ev->type = SDL_KEYDOWN;
+
+  switch(strlen(key))
+  {
+    case 1:
+      char c=key[0];
+      if(c>='1'&&c<='9')
+        ev-> key.keysym.sym= 15+c-'1';
+      else if(c=='0')
+        ev->key.keysym.sym= SDLK_0;
+      else if(c=='J')
+        ev->key.keysym.sym=SDLK_J;
+      else if(c=='K')
+        ev->key.keysym.sym=SDLK_K;
+      else if(c=='G')
+        ev->key.keysym.sym=SDLK_G;
+      break;
+    case 2:
+      ev->key.keysym.sym=SDLK_UP;
+      break;
+    case 4:
+      if(key[0]=='D')
+        ev->key.keysym.sym=SDLK_DOWN;
+      else if(key[0]=='L')
+        ev->key.keysym.sym=SDLK_LEFT;
+      else if(key[0]=='R')
+        ev->key.keysym.sym=SDLK_RIGHT;
+      else
+        ev->key.keysym.sym=SDLK_NONE;
+      break;
+    default:
+      ev->key.keysym.sym=SDLK_NONE;
+  }
+  
+  if(ev->type=SDL_KEYDOWN)
+    printf("type:%s key:%s\n",type,key);
+  return 1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
@@ -61,8 +107,9 @@ int SDL_WaitEvent(SDL_Event *event) {
     default:
       event->key.keysym.sym=SDLK_NONE;
   }
-
   }
+  if(event->type=SDL_KEYDOWN)
+    printf("type:%s key:%s\n",type,key);
   return 1;
 }
 
