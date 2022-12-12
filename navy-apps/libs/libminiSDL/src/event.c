0c15d74862760053424fa914a6016161bebeb2e9
tracer-ics2022 <tracer@njuics.org>
2022-12-12 09:37:39 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:37:39 up 14 min,  1 user,  load average: 1.20, 0.85, 0.57

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -61,8 +61,8 @@ int SDL_WaitEvent(SDL_Event *event) {
     default:
       event->key.keysym.sym=SDLK_NONE;
   }
-
-  printf("type:%s key:%s\n",type,key);
+  if(event->type=SDL_KEYDOWN)
+    printf("type:%s key:%s\n",type,key);
   }
   return 1;
 }
