4528ce85759ceb9792e8991ef3b5795717952df7
tracer-ics2022 <tracer@njuics.org>
2022-12-15 14:27:32 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:27:32 up 16 min,  1 user,  load average: 0.76, 0.63, 0.38

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,7 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include <stdio.h>
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -12,19 +13,20 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  FILE* fp=fopen(filename, "r");
-  fseek(fp, 0, SEEK_END);
-  int size=ftell(fp);
+  FILE *img = fopen(filename, "r");
+  fseek(img, 0, SEEK_END);
+  size_t size = ftell(img);
 
-  char* buf=malloc(size*sizeof(char));
-  fseek(fp, 0, SEEK_SET);
-  printf("seek %d\n", size);
-  fread(buf, 1, size, fp);
+  char *img_data = malloc(sizeof (char) * size);
+  fseek(img, 0, SEEK_SET);
+  fread(img_data, size, 1, img);
+
+  SDL_Surface *img_surface = STBIMG_LoadFromMemory(img_data, size);
+
+  free(img_data);
   
-  SDL_Surface* sur=STBIMG_LoadFromMemory(buf,size);
-  fclose(fp);
-  free(buf);
-  return sur;
+  fclose(img);
+  return img_surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
