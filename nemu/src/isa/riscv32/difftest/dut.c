eeaac889b9eee397fade5d07628b7e2b1ed830db
tracer-ics2022 <tracer@njuics.org>
2022-12-21 22:43:54 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:43:54 up 50 min,  1 user,  load average: 0.56, 0.42, 0.39

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -21,9 +21,11 @@ extern CPU_state cpu;
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i=0;i<32;i++)
     if(ref_r->gpr[i]!=cpu.gpr[i])
-      return false;
+    { printf("diff: reg%d mismatch\n",i);
+      return false;}
   if(ref_r->pc!=cpu.pc)
-    return false;
+  { printf("diff: PC mismatch\n");
+    return false;}
   return true;
 }
 
