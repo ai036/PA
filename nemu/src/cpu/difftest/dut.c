1ebee004b0c3eb747c591303136189fa735c74fd
tracer-ics2022 <tracer@njuics.org>
2022-12-21 22:54:27 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:54:27 up  1:01,  1 user,  load average: 0.54, 0.48, 0.42

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -35,7 +35,7 @@ bool is_difftest();
 int copy_into_ref()//进入difftest，从dut复制信息到ref
 {
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), PMEM_RIGHT-PMEM_LEFT, DIFFTEST_TO_REF);
+
   return 0;
 }
 
