a46fc226f7ca58d98d28afa81ebc039bd2121fca
tracer-ics2022 <tracer@njuics.org>
2022-12-22 18:43:25 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:43:25 up 6 min,  1 user,  load average: 0.23, 0.37, 0.20

--- nemu/src/cpu/difftest/dut.c
+++ nemu/src/cpu/difftest/dut.c
@@ -26,18 +26,18 @@ void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
-#ifdef CONFIG_DIFFTEST
-
-static bool is_skip_ref = false;
-static int skip_dut_nr_inst = 0;
 bool is_difftest();
-
 int copy_into_ref()//进入difftest，从dut复制信息到ref
 {
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), PMEM_RIGHT-PMEM_LEFT, DIFFTEST_TO_REF);
   return 0;
 }
+
+#ifdef CONFIG_DIFFTEST
+
+static bool is_skip_ref = false;
+static int skip_dut_nr_inst = 0;
 
 // this is used to let ref skip instructions which
 // can not produce consistent behavior with NEMU
