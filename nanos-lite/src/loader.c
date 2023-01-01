85cd07ca341688817635e0018f0e820fdc3f9482
tracer-ics2022 <tracer@njuics.org>
2023-01-01 21:27:19 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:19 up  2:02,  1 user,  load average: 0.04, 0.27, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -67,7 +67,7 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
   int envc=0,argc=0;
   char* brk=(char*)heap.end;
   if(argv)
-  for(;argv[argc]!=NULL;argc++)
+  for(;argv[argc];argc++)
   { printf("%s: \n", argv[argc]);
 ;
    }printf("HHHHHH%d\n",argc);
