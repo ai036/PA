7d397725322a605bad065319d349396a7c3126df
tracer-ics2022 <tracer@njuics.org>
2023-01-01 21:15:27 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:27 up  1:50,  1 user,  load average: 0.53, 0.59, 0.47

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  printf("args:%s\n",args);
+
   char *empty[] =  {NULL };
   environ = empty;
   exit(main(0, empty, empty));
