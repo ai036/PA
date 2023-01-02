1f69c81e1c0627451460336619e73dc636004d1d
tracer-ics2022 <tracer@njuics.org>
2023-01-02 16:54:07 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:54:07 up 42 min,  1 user,  load average: 0.49, 0.42, 0.44

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -25,13 +25,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   for(int i=0;i<elf_head.e_phnum;i++)
     if(phdr[i].p_type==PT_LOAD)
-    { printf("load %s\n",filename);
-
+    { 
       fs_lseek(fd,phdr[i].p_offset,SEEK_SET);
       fs_read(fd,(void*)phdr[i].p_vaddr,phdr[i].p_memsz);
       memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz),0,phdr[i].p_memsz-phdr[i].p_filesz);
     }
   fs_close(fd);
+  printf("load %s\n",filename);
   printf("entry: %p\n",elf_head.e_entry);
   return elf_head.e_entry;
 }
