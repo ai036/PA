4aa92de3d2f6bd61deecaaac3f9a3eb9e5b8f52c
tracer-ics2022 <tracer@njuics.org>
2023-01-02 11:21:53 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:21:53 up 33 min,  1 user,  load average: 0.70, 0.65, 0.56

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -37,7 +37,7 @@ void context_kload(PCB* p,void (*entry)(void *), void *arg)
 
 void init_proc() {
   context_kload(&pcb[0], hello_fun, "hhr");
-  char *v[]={"234",NULL};
+  char *v[]={NULL};
   context_uload(&pcb[1], "/bin/pal",v,NULL);
   
   switch_boot_pcb();
