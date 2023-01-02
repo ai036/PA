d1c2ef1d3175cc62eb2d19b840f70d6157fa714b
tracer-ics2022 <tracer@njuics.org>
2023-01-02 12:09:45 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-56-generic #62-Ubuntu SMP Tue Nov 22 19:54:14 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:09:45 up  1:21,  1 user,  load average: 0.72, 0.91, 0.72

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -29,16 +29,7 @@ static void sh_handle_cmd(const char *cmd) {
   char*file=strtok(command, " ");
 
 
-  char* argv[15];
-  int argc=0;
-  char* token=strtok(NULL, " ");
-  
-   /* 读取参数列表argv[] */
-  while( token != NULL ) {
-      printf( "%s\n", token);
-      token = strtok(NULL, " ");
-      strcpy(argv[argc++],token);
-   }//BUGY    
+
   execvp(file,0);
 }
 
