ebfc2905c36f7472a07b65b64aad6a69413f53e2
tracer-ics2022 <tracer@njuics.org>
2023-01-09 09:16:09 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:16:09 up  1:07,  1 user,  load average: 0.53, 0.53, 0.46

--- abstract-machine/am/src/riscv/nemu/vme.c
+++ abstract-machine/am/src/riscv/nemu/vme.c
@@ -39,11 +39,11 @@ bool vme_init(void* (*pgalloc_f)(int), void (*pgfree_f)(void*)) {
     }
     
   }
-  printf("vme_init\n");
+
   set_satp(kas.ptr);
-
   vme_enable = 1;
-
+  
+  printf("vme_init\n");
   return true;
 }
 
