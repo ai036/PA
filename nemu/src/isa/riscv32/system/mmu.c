3ef24900f773ebce862cdf1465a132132cdb202b
tracer-ics2022 <tracer@njuics.org>
2023-01-09 11:58:15 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:58:15 up 18 min,  1 user,  load average: 0.52, 0.41, 0.55

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -17,8 +17,27 @@
 #include <memory/paddr.h>
 #include <memory/vaddr.h>
 
+static inline uintptr_t VPN1(uintptr_t addr)
+{
+  return (addr & 0xffc00000u)>>22;
+}
+
+static inline uintptr_t VPN0(uintptr_t addr)
+{
+  return (addr & 0x003ff000u)>>12;
+}
+
+#define OFFSET 0x00000fffu
+#define PPN 0xfffffc00u
+#define PTE_SIGN 0x000003ffu
+
+#define PTE_V 0x01
+
+typedef uintptr_t PTE;
+
 //TODO: 实现地址翻译
 paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
-
+  PTE* pte=(PTE*)((csr.satp << 12)+ VPN1(vaddr)*4);
+  assert(*pte & PTE_V);
   return vaddr;
 }
