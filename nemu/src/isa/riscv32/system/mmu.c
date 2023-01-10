a6ab33aca4aeefbc627b3cb6c1ab007a526751da
tracer-ics2022 <tracer@njuics.org>
2023-01-10 20:25:16 +0800

    >  run NEMU 211300036 郝浩然 Linux hhr-virtual-machine 5.15.0-57-generic #63-Ubuntu SMP Thu Nov 24 13:43:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:16 up  5:03,  1 user,  load average: 0.45, 0.54, 0.47

--- nemu/src/isa/riscv32/system/mmu.c
+++ nemu/src/isa/riscv32/system/mmu.c
@@ -44,8 +44,14 @@ paddr_t isa_mmu_translate(vaddr_t vaddr, int len, int type) {
 
   paddr_t leaf_pte_addr= ((pte & PPN) << 2) + VPN0(vaddr)*4;
   PTE leaf_pte=paddr_read(leaf_pte_addr,4);//叶节点页表项
+  if((leaf_pte & PTE_V)==0)
+    printf("non valid leaf_pte=%lx, leaf_pte_addr=%x\n",leaf_pte,leaf_pte_addr);
   assert(leaf_pte & PTE_V);//检查页表项的valid位
-
+  if (type == 0){//读
+    paddr_write(leaf_pte_addr, 4, leaf_pte | 0x40);
+  }else if (type == 1){//写
+    paddr_write(leaf_pte_addr, 4, leaf_pte | 0x80);
+  }
   paddr_t paddr=((leaf_pte & PPN) << 2) | (vaddr & OFFSET);//物理地址
 
   //assert(paddr==vaddr);
